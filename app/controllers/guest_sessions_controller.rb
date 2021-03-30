8ef42dbf29e39ab7723b11a3c3fa876c165d8da8
akari <akarikatonufs@gmail.com>
2021-03-30 11:12:53 +0000

    ゲストログイン機能

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -1,7 +1,6 @@
 class GuestSessionsController < ApplicationController
   
   def create
-    binding.pry
     user = User.find_by(email: ENV['TEST_EMAIL'])
     user && user.authenticate(password: ENV['TEST_PASSWORD'])
     log_in(user)

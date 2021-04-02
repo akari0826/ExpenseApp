6e4d6e5eb7c3749c347411a4f3953e258f85e077
akari <akarikatonufs@gmail.com>
2021-04-02 11:55:42 +0000

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?, :current_user?
   add_flash_types :success, :info, :warning, :danger
   
-  
   def set_current_user
     @current_user = User.find_by(id: session[:user_id])
   end

924dfb8be41e42bb0842ca4fe3833f83d0c358a9
akari <akarikatonufs@gmail.com>
2021-05-14 14:19:14 +0000

    テストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "名前、メールアドレス、パスワードがあれば有効な状態であること"
+  it "名前、メールアドレス、パスワードがあれば有効な状態であること" do
+    user = User.new(
+      name: "加藤夏樹",
+      email: "katonatsuki9@gmail.com",
+      password: "mypassword123",
+      )
+    expect(user).to be_valid
+  end
   it "名前がなければ無効な状態であること"
   it "メールアドレスがなければ無効な状態であること"
   it "パスワードがなければ無効な状態であること"

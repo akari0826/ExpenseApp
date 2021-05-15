f3f7e0f5dfeeeed02162bd4614b96027ee00e46c
akari <akarikatonufs@gmail.com>
2021-05-15 11:55:31 +0000

    テストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,8 +9,28 @@ RSpec.describe User, type: :model do
       )
     expect(user).to be_valid
   end
-  it "名前がなければ無効な状態であること"
-  it "メールアドレスがなければ無効な状態であること"
+  
+  it "名前がなければ無効な状態であること" do
+    user = User.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("を入力してください")
+  end
+  
+  it "メールアドレスがなければ無効な状態であること" do
+    User.create(
+      name: "加藤夏樹",
+      email: "katonatsuki9@gmail.com",
+      password: "mypassword123",
+      )
+    user = User.new(
+      name: "加藤夏樹",
+      email: "katonatsuki9@gmail.com",
+      password: "mypassword123",
+      )
+    user.valid?
+    expect(user.errors[:email]).to include("既に使われています")
+  end
+  
   it "パスワードがなければ無効な状態であること"
   it "重複したメールアドレスなら無効な状態であること"
   it "ユーザのフルネームを文字列として返すこと"

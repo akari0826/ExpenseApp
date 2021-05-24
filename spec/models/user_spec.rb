da9e12b87ffcd711a39becb4bd24c6ee65e2496a
akari <akarikatonufs@gmail.com>
2021-05-24 13:38:09 +0000

    テストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -56,13 +56,45 @@ RSpec.describe User, type: :model do
   
   it "メールアドレスの正規表現が適切なら有効な状態であること"
   it "メールアドレスの正規表現が不適切なら無効な状態であること"
+  
   it "パスワードがなければ無効な状態であること" do
     user = User.new(password: nil)
     user.valid?
     expect(user.errors[:password]).to include("を入力してください")
   end
-  it "パスワードが8〜32文字以内なら有効な状態であること"
-  it "パスワードが8〜32文字以上なら無効な状態であること"
+  
+  it "パスワードが8〜32文字以内なら有効な状態であること" do
+    user = User.new(
+      name: "加藤夏樹",
+      email: "katonatsuki9@gmail.com",
+      password: "mypassword123",
+      password_confirmation: "mypassword123"
+      )
+    expect(user).to be_valid
+  end
+  
+  it "パスワードが8文字以内なら無効な状態であること" do
+    user = User.new(
+      name: "加藤和樹",
+      email: "katonatsuki9@gmail.com",
+      password: "mypass1",
+      password_confirmation: "mypass1"
+      )
+    user.valid?
+    expect(user.errors[:password]).to include("は8文字以上で入力してください")
+  end
+  
+  it "パスワードが32文字以上なら無効な状態であること" do
+    user = User.new(
+      name: "加藤和樹",
+      email: "katonatsuki9@gmail.com",
+      password: "mypass111111111111111111111111111",
+      password_confirmation: "mypass111111111111111111111111111"
+      )
+    user.valid?
+    expect(user.errors[:password]).to include("は32文字以内で入力してください")
+  end
+  
   it "パスワードの正規表現が適切なら有効な状態であること"
   it "パスワードの正規表現が不適切なら無効な状態であること"
   # it "パスワード（確認用）がなければ無効な状態であること" do

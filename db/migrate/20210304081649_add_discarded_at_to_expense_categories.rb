8a4d83ba32eade00bab48deb3351d6af6ef84003
akari <akarikatonufs@gmail.com>
2021-05-29 08:29:50 +0000

    修正

--- db/migrate/20210304081649_add_discarded_at_to_expense_categories.rb
+++ db/migrate/20210304081649_add_discarded_at_to_expense_categories.rb
@@ -1,6 +1,6 @@
 class AddDiscardedAtToExpenseCategories < ActiveRecord::Migration[5.2]
   def change
-    change_table :expenses_categories, bulk: true do |t|
+    change_table :expense_categories, bulk: true do |t|
       t.datetime :discarded_at
     end
     add_index :expense_categories, :discarded_at

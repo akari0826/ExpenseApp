d0b003f23bac30beb8b263968773af8e61a6d091
akari <akarikatonufs@gmail.com>
2021-06-16 06:47:55 +0000

    RSpecテストコード

--- spec/models/expense_category_spec.rb
+++ spec/models/expense_category_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.default ExpenseCategory, type: :model do
-  
+  it "カテゴリ名があれば有効な状態であること"
+  it "カテゴリ名がなければ無効な状態であること"
+  it "名前が20文字以内なら有効な状態であること"
+  it "名前が20文字以上なら無効な状態であること"
 end
